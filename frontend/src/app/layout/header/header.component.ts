57f5ecc28e2f9670827577f8d026dca9dfcc1dde
Bashar <mm2mm1mm4@gmail.com>
2026-07-14 15:55:35 +0300

    U

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -38,7 +38,7 @@ export class HeaderComponent {
       label: 'الخدمات الرقمية',
       path: '/projects',
       children: [
-        { label: 'تسجيل وتجديد العضوية', path: 'https://docs.google.com/forms/d/e/1FAIpQLSdUx_xMMNzjGqXo9O7vkzssZj1UIUaSozn47c5qDNOdcetOFg/viewform' },
+        { label: 'تسجيل وتجديد العضوية', path: 'https://e.barbarcharity.org/OnlineServices/mmb_online.php' },
         { label: 'طلب مساعدة إجتماعية', path: 'https://e.barbarcharity.org/OnlineServices/aid_soc.php' },
         { label: 'مشروع كنوز العطاء', path: 'https://docs.google.com/forms/d/e/1FAIpQLSftIxJWNzpzFe-UXMc5-cPEy_W8FaIUdNr6LHj7TPO01cTWEg/viewform' },
       ],
